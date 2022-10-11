86e2572fa51f83ba883c3148c3931da9cf1fa931
JamesWithCode <minseok.jeong.james@gmail.com>
2022-10-11 20:14:13 +0900

    221011 백트래킹

--- 백트래킹 (Backtracking)/boj_.py
+++ 백트래킹 (Backtracking)/boj_.py
@@ -3,8 +3,5 @@ sys.stdin = open("input.text",  "rt")
 import sys
 input=sys.stdin.readline
 
-def solution():
-    answer=-1
-    return answer
 
 if __name__=="__main__":
---
+++ 백트래킹 (Backtracking)/boj_1189.py
@@ -0,0 +1,24 @@
+import sys
+sys.stdin = open("input.text",  "rt")
+import sys
+input=sys.stdin.readline
+
+def dfs(y,x,cnt):
+    global res
+    if y==0 and x==c-1 and cnt==k-1:res+=1;return
+    if cnt>=k:return
+    for dy,dx in zip([-1,0,1,0],[0,1,0,-1]):
+        ny,nx=y+dy,x+dx
+        if 0<=ny<r and 0<=nx<c and not ch[ny][nx] and mat[ny][nx] !='T':
+            ch[ny][nx]=1
+            dfs(ny,nx,cnt+1)
+            ch[ny][nx]=0
+
+
+if __name__=="__main__":
+    r,c,k=map(int,input().split())
+    mat=[list(input().strip()) for _ in range(r)]
+    ch=[[0]*c for _ in range(r)]; ch[r-1][0]=1
+    res=0
+    dfs(r-1,0,0)
+    print(res)
