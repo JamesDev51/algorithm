8ef579fc5762aeac2e9e068047639942c2033d77
JamesWithCode <minseok.jeong.james@gmail.com>
2022-11-06 13:41:34 +0900

    221106 구간합배열

--- 구간합 배열 (Prefix Sum)/boj_10211.py
+++ 구간합 배열 (Prefix Sum)/boj_10211.py
@@ -11,7 +11,6 @@ if __name__=="__main__":
         p_sum=[0]*(n+1)
         for i in range(1,n+1): p_sum[i]=p_sum[i-1]+arr[i-1]
         
-        print(p_sum)
         largest=float('-inf')
         for i in range(1,n+1):
             for j in range(i,n+1):
