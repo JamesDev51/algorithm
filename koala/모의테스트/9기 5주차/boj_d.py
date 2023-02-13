a596f13e2198959d36cc65dec884ab375d4b824d
jeongminseok <minseok.jeong.james@gmail.com>
2023-02-13 13:59:33 +0900

    230213 백준 koala 모의테스트 5주차 2문제

--- koala/모의테스트/9기 5주차/boj_d.py
+++ koala/모의테스트/9기 5주차/boj_d.py
@@ -1,7 +1,45 @@
 import sys
 sys.stdin = open("input.text",  "rt")
 import sys
+from collections import deque
 input=sys.stdin.readline
 
 if __name__=="__main__":
-    
+    for _ in range(int(input())):
+        k,m,p=map(int,input().split())
+        tails=[0]*(m+1)
+        graph=[[] for _ in range(m+1)]
+        for _ in range(p):
+            a,b=map(int,input().split())
+            graph[a].append(b)
+            tails[b]+=1
+        que=deque()
+        strahler_tails=[[0,0] for _ in range(m+1)]
+        for node in range(1,m+1):
+            if not tails[node]:
+                que.append((node,1))
+                strahler_tails[node]=[1,1]
+
+        while que:
+            node,strahler = que.popleft()
+            
+            for next_node in graph[node]:
+                tails[next_node]-=1
+                if strahler>strahler_tails[next_node][0]:strahler_tails[next_node]=[strahler,1]
+                elif strahler==strahler_tails[next_node][0]:strahler_tails[next_node][1]+=1
+                
+                if not tails[next_node]:
+                    if strahler_tails[next_node][1]>=2:que.append((next_node,strahler_tails[next_node][0]+1))
+                    else:que.append((next_node,strahler_tails[next_node][0]))
+        
+        res=float('-inf')
+        for node in range(1,m+1):
+            st,cnt=strahler_tails[node]
+            if cnt>=2:res=max(res,st+1)
+            else:res=max(res,st)
+        print(k,res)
+                
+                
+                
+                
+                                
--- koala/모의테스트/9기 5주차/boj_e.py
+++ koala/모의테스트/9기 5주차/boj_e.py
@@ -2,6 +2,36 @@ import sys
 sys.stdin = open("input.text",  "rt")
 import sys
 input=sys.stdin.readline
+import heapq
 
+def dijkstra():
+    heap=[]
+    heapq.heappush(heap,(0,2))
+    while heap:
+        dist,node=heapq.heappop(heap)
+        if distance[node]<dist:continue
+        for next_node,next_dist in graph[node]:
+            new_dist=dist+next_dist
+            if new_dist<distance[next_node]:
+                distance[next_node]=new_dist
+                heapq.heappush(heap,(new_dist,next_node))
+            if distance[node]<distance[next_node]:
+                dp[next_node]+=dp[node]
+# def go(node):
+#     if dp[node]!=-1: return dp[node]
+#     dp[node]=0
+#     for next_node, _ in graph[node]:
+#         if distance[node]>distance[next_node]:
+#             dp[node]+=go(next_node)
+#     return dp[node]
 if __name__=="__main__":
-    
+    n,m=map(int,input().split())
+    graph=[[] for _ in range(n+1)]
+    dp=[0]* (n+1);dp[2]=1
+    for _ in range(m):
+        a,b,c=map(int,input().split())
+        graph[a].append((b,c))
+        graph[b].append((a,c))
+    distance=[float('inf')]*(n+1); distance[2]=0
+    dijkstra()
+    print(dp[1])
